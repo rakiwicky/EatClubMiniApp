2a00bce1cd696580cbdeb213aab46e9ba83460ea
Rakhita Wickramatunge <rakiwicky@gmail.com>
2025-08-18 12:02:17 +1000

    Initial project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EatClubMiniApp"
+include(":app")
+include(":feature-resturants")
+include(":library-common-compose")
+include(":library-network")
